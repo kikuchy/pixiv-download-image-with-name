aeea6087e54254797b34c8fc7c471a141be8a52e
kikuchy <kikuchy.h@gmail.com>
2013-09-25 11:47:26 +0900

    editing option page

--- options.js
+++ options.js
@@ -12,9 +12,9 @@ var changeEventHandler = function(e){
 var illustname = document.getElementById("illustname");
 illustname.addEventListener('change', changeEventHandler, false);
 illustname.addEventListener('keydown', changeEventHandler, false);
-illustname.value = settings.client || "{illustTitle} - {userName} - {illustId}";
+illustname.value = settings.illustname || "{illustTitle} - {userName} - {illustId}";
 
 var manganame = document.getElementById("manganame");
 manganame.addEventListener("change", changeEventHandler, false);
 manganame.addEventListener("keydown", changeEventHandler, false);
-manganame.value = settings.screenname || "{illustTitle} - {userName} - {illustId}_{page}";
+manganame.value = settings.manganame || "{illustTitle} - {userName} - {illustId}_{page}";
